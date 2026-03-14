e0bd963b4b8c1c5b988596668033472e0fc1a76d
Koichi555 <112304480+Koichi555@users.noreply.github.com>
2026-03-14 10:45:19 +0800

    Create config_service.py

---
+++ backend/app/services/config_service.py
@@ -0,0 +1,5 @@
+def get_loads():
+    return [
+        {"tagKey": "Panel01::Light01", "name": "Barn A Light 01", "group": "Barn A", "writable": True},
+        {"tagKey": "Panel01::Light02", "name": "Barn A Light 02", "group": "Barn A", "writable": True},
+    ]
